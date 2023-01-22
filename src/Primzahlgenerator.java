5672908bf572c9b841d9be0f3f940b977832c093
Peter Dillinger <peter.dillinger@email.de>
2023-01-22 13:13:06 +0100

    Wie man eine Klasse baut, die Primzahlen generieren kann

--- src/Primzahlgenerator.java
+++ src/Primzahlgenerator.java
@@ -2,23 +2,11 @@
 // Entwerfen Sie eine Klasse, die Primzahlen erzeugen kann, beginnend bei einem Startwert.
 // Nachfolgende Aufrufe derselben Methode liefern jeweils die nächstfolgenden Primzahlen.
 
-// Beginn einer Lösung:
 public class Primzahlgenerator {
 
     // TODO
-    // Machen Sie eine richtige Klasse daraus, d.h. ergänzen Sie
-    // erstmal notwendige Attribute und einen entsprechenden Konstruktor!
-    // Hinweis: Die Aufgabenstellung spricht von einem "Startwert" für
-    // den Generator. Es spricht nichts dagegegen, das Attribut auch so
-    // zu nennen
-
-    // statische Methode als Helfer, nutzbar ohne Instanz der Klasse
-    // (Klassenmethode)
-    public static boolean istPrimzahl(int zahl) {
-        for (int teiler = 2; teiler < zahl; teiler++) {
-            if (zahl % teiler == 0) return false;
-        }
-        return true;
-    }
-
+    // Bauen Sie zuerst eine statische Methode ein, mit der man eine
+    // beliebige Ganzzahl prüfen kann, ob sie eine Primzahl ist.
+    // Hinweis: Benutzen Sie dazu das einfache Verfahren, das wir eingesetzt haben,
+    // als wir uns mit Schleifen und Entscheidungen beschäftigt haben.
 }
